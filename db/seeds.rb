d97c0900a6b28874c3ed791135073c883fe3d9b5
github username <github email address>
2019-11-11 18:24:12 -0500

    created additional seed data to test against

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,8 @@ taylor_swift = Artist.find_or_create_by(name: "Taylor Swift")
 anthem = Venue.find_or_create_by(name: "Anthem", address: "123 Main Street",city: "Washington DC", state: "District")
 fedex_field = Venue.find_or_create_by(name: "Fedex Field", address: "1 Fedex Field Road", city: "NFL", state: "MD")
 jiffy_lube = Venue.find_or_create_by(name: "Jiffy Lube Arena", address: "342 Jiffy Lube Street", city: "Fairfax", state: "VA")
+pearl_street_warehouse = Venue.find_or_create_by(name: "Park Street Warehouse", address: "33 Pearl Street", city: "Washington DC", state: "DC")
+the_hamilton = Venue.find_or_create_by(name: "The Hamilton", address: "600 14th St NW", city: "Washington DC", state: "DC")
 
 event1 = Event.find_or_create_by(artist_id: 1, venue_id: 2, date: "nil")
 event2 = Event.find_or_create_by(artist_id: 3, venue_id: 3, date: "nil")
