9b515c03ed7d368eb4ff861dc15318a9debd3340
github username <github email address>
2019-11-12 14:53:36 -0500

    added functionality to return to start menu

--- lib/application.rb
+++ lib/application.rb
@@ -4,20 +4,26 @@
     def artist_sub_menu
         puts "Artist Results Loaded!"
         puts "Please type an Artist name(Beyonce, Queen)"
+        puts "Type Quit to return to main screen."
         input = gets.chomp
         if input == "Queen"
             puts Artist.where(name: "Queen")
         elsif input == "Beyonce"
             puts Artist.where(name: 
             "Beyonce")
+            elsif input == "Quit"
+                return start_menu
         else
-            puts "Invalid Input"
+            puts "Invalid Input. Please Try Again."
+            return artist_sub_menu
         end
     end
 
+    
     def location_sub_menu
         puts "Location Results Loaded"
         puts "Please type a Location(MD, VA, DC)"
+        puts "Type Quit to return to main screen."
         input = gets.chomp
         if input == "VA"
             puts Venue.where(state: "VA")
@@ -25,8 +31,11 @@
             puts Venue.where(state: "DC")
         elsif input == "MD"
             puts Venue.where(state: "MD")
+        elsif input == "Quit"
+            return start_menu
         else
-            puts "Invalid Input"
+            puts "Invalid Input. Please Try Again."
+            return location_sub_menu
         end
     end
 
@@ -34,13 +43,17 @@
     def venue_sub_menu
         puts "Venue Results Loaded"
         puts "Please type a Venue(Anthem, Fedex Field)"
+        puts "Type Quit to return to main screen."
         input = gets.chomp
         if input == "Anthem"
             puts Venue.where(name: "Anthem")
         elsif input == "Fedex Field"
             puts Venue.where(name: "Fedex Field")
+        elsif input == "Quit"
+            return start_menu
         else
-            puts "Invalid Input"
+            puts "Invalid Input. Please Try Again."
+            return venue_sub_menu
         end
     end
     
@@ -49,18 +62,17 @@
         string = gets.chomp
         if string == "Artist"
             return artist_sub_menu
-            
         elsif string == "Venue"
             return venue_sub_menu
         elsif string == "Location"
             return location_sub_menu
-        
         else
-            puts "Invalid Input"
+            puts "Invalid Input. Please Try Again."
+            return start_menu
         end
     end
 
-    start_menu 
+start_menu 
    
     
 
