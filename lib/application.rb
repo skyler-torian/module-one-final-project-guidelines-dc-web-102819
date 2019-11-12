1aa7570d3a134b0872aec8d71787157112d9f053
github username <github email address>
2019-11-12 13:01:52 -0500

    added conditional sub menus

--- lib/application.rb
+++ lib/application.rb
@@ -1,41 +1,54 @@
-# class CLI < Thor
-#     desc "Artist", "Venue"
-#     def hello(name)
-#       if name == ""
-#         puts "Artist is #{name}"
-#       else
-#         puts "Try again"
-#        end
-#     end
-#   end
 
-# CLI.start
-# #(ARGV)
-
-
-    def start
-        puts "Please select an option." 
-        puts "Type Artist to inspect an Artist playing a show in the area."
-        puts "Type Venue to inspect Venues in the area with upcoming shows"
-        artist_selection = "Artist"
-        venue_selection = "Venue"
-        user_input = gets.chomp
-        if user_input == artist_selection
-            puts "You selected Artists. Loading results"
-        elsif user_input == venue_selection
-            puts "You selected Venues. Loading results"
+    def artist_sub_menu
+        puts "Artist Results Loaded!"
+        puts "Please type an Artist name(Beyonce, Queen)"
+        input = gets.chomp
+        if input == "Queen"
+            puts "I like Queen too"
+        elsif input == "Beyonce"
+            puts "Bey is good too"
         else
             puts "Invalid Input"
+        end
     end
 
-    def artist_sub_page
+
+    def venue_sub_menu
+        puts "Venue Results Loaded"
+        puts "Please type a Venue(Anthem, Fedex Field)"
+        input = gets.chomp
+        if input == "Anthem"
+            puts "Anthem is located at the Wharf in downtown Washington DC"
+        elsif input == "Fedex Field"
+            puts "Fedex Field is the home of mega concerts and the Washington Redskins. It is located in Maryland"
+        else
+            puts "Invalid Input"
+        end
+    end
+    
+    def start_menu
+        puts "Please type an option(Artist, Venue)"
+        string = gets.chomp
+        if string == "Artist"
+            return artist_sub_menu
+            
+        elsif string == "Venue"
+            return venue_sub_menu
         
+        else
+            puts "Invalid Input"
+        end
     end
 
-    def venue_sub_page
-    end
+    start_menu 
+   
+    
 
-end
 
-    start
+    
+
+
+
+
+    
    
