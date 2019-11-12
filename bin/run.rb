fe96ffe7efa4a154afd8a1724daf95accc52ebbf
github username <github email address>
2019-11-12 12:23:33 -0500

    working inputs and outputs in starting menu

--- bin/run.rb
+++ bin/run.rb
@@ -3,5 +3,5 @@ require_relative '../config/environment'
 
 
 # puts "HELLO WORLD"
-CLI.start(ARGV)
+
 # binding.pry
--- lib/application.rb
+++ lib/application.rb
@@ -15,9 +15,27 @@
 
     def start
         puts "Please select an option." 
-        puts "Press 1 to inspect an Artist playing a show in the area."
-        puts "Press 2 to inspect Venues in the area with upcoming shows"
+        puts "Type Artist to inspect an Artist playing a show in the area."
+        puts "Type Venue to inspect Venues in the area with upcoming shows"
+        artist_selection = "Artist"
+        venue_selection = "Venue"
         user_input = gets.chomp
+        if user_input == artist_selection
+            puts "You selected Artists. Loading results"
+        elsif user_input == venue_selection
+            puts "You selected Venues. Loading results"
+        else
+            puts "Invalid Input"
     end
 
+    def artist_sub_page
+        
+    end
+
+    def venue_sub_page
+    end
+
+end
+
+    start
    
